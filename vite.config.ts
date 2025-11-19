e890fa80b9ba3242911f113ce52af8cb4e86fc18
keeterly <30454504+keeterly@users.noreply.github.com>
2025-11-19 10:09:56 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/dream/",
+  build: {
+    target: ["safari13", "es2019"], // <-- ADD THIS
+  },
 });
